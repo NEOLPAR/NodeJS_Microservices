2c3889b7b004b66e2e16d1607d5e78f0a724f2b2
Ricardo Del Rio Rubial <ricardo@neolpar.com>
2020-12-08 20:27:30 +0000

    Ch5. Factoring out the feedback service

--- provided/conference-app/server/config/index.js
+++ provided/conference-app/server/config/index.js
@@ -5,16 +5,10 @@ module.exports = {
     sitename: 'Roux Meetups [Development]',
     serviceRegistryUrl: 'http://localhost:3000',
     serviceVersionIdentifier: '1.x.x',
-    data: {
-      feedback: path.join(__dirname, '../data/feedback.json'),
-    },
   },
   production: {
     sitename: 'Roux Meetups',
     serviceRegistryUrl: 'http://localhost:3000',
     serviceVersionIdentifier: '1.x.x',
-    data: {
-      feedback: path.join(__dirname, '../data/feedback.json'),
-    },
   },
 };
